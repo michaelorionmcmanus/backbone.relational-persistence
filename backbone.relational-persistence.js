1318ee71286f0662ac3d8fc5fbee588f058fe4db
Michael McManus <michaelorionmcmanus@gmail.com>
2013-03-19 10:46:49 -0700

    Added support for testing dirty state.

--- backbone.relational-persistence.js
+++ backbone.relational-persistence.js
@@ -5,12 +5,83 @@
 var Backbone = window.Backbone;
 var _ = window._;
 var $ = window.$;
+var OriginalConstructor = Backbone.RelationalModel.prototype.constructor;
 
-// Override relational with the methods we want.
+/**
+@class RelationalPersistence
+@extends Backbone.RelationalModel
+*/
 var RelationalPersistence = Backbone.RelationalModel.extend({
-	isDirty: function() {
-		return true;
-	}
+  /**
+    Constructor override
+
+    @method constructor
+    @for RelationalPersistence
+  **/
+	constructor: function() {
+    // Bind to the sync even to set original attributes.
+    this.on('sync', this.setOriginalAttributes);
+    // Bind to the destroy event to remove sync binding.
+		this.once('destroy', this.unbindOriginalAttributes);
+    // Call the parent constructor
+		OriginalConstructor.apply(this, arguments);
+	},
+
+  /**
+    The model's original attributes. Initialized in
+    {{#crossLink "RelationalPersistence/setOriginalAttributes:method"}}{{/crossLink}}
+
+    @protected
+    @property _originalAttributes
+  **/
+  _originalAttributes: [],
+
+  /**
+    Set the original attributes on the model. Typically called when the model is synced with
+    the remote data store.
+
+    @method setOriginalAttributes
+    @return {void}
+  **/
+  setOriginalAttributes: function() {
+    this._originalAttributes = _.clone(this.attributes);
+  },
+
+  /**
+    Unbinds the call to {{#crossLink "RelationalPersistence/setOriginalAttributes:method"}}{{/crossLink}} from 
+    the "sync" event.
+
+    @method unbindOriginalAttributes
+    @return {void}
+  **/
+  unbindOriginalAttributes: function() {
+    this.off('sync', this.setOriginalAttributes);
+  },
+
+  /**
+    Restore the last known "clean" set of attributes. Use `Backbone.Model.prototype.set` to restore the 
+    attributes since `Backbone.RelationalModel` overrides `set` to handle relations. 
+
+    @method restoreOriginalAttributes
+    @return {void}
+  **/
+  restoreOriginalAttributes: function() {
+    // Call set using the Backbone.Model prototype, since we arent' concerned with updating relations. 
+    // If we end up needing to reset relations, we should probably act on those independently.
+    Backbone.Model.prototype.set.call(this, this._originalAttributes);
+  },
+
+  /**
+    Check to see whether or not the model is dirty. Compares {{#crossLink "RelationalPersistence/_originalAttributes:property"}}{{/crossLink}}
+    to `this.attributes`.
+
+
+    @method isDirty
+    @return {boolean} `true` if the model is dirty, `false` otherwise.
+  **/
+  isDirty: function() {
+    return !_.isEqual(this.attributes, this._originalAttributes);
+  },
 });
 
 // Export!
--- test/dirty.js
+++ test/dirty.js
@@ -17,8 +17,98 @@ QUnit.module("dirty tools", {
 });
 
 // Ensure the correct defaults are set.
-test("defaults", 1, function() {
+test("defaults", 2, function() {
   var model = new this.Model();
   ok(_.isFunction(model.isDirty), "isDirty is a method on a Backbone.RelationalModel");
+  ok(_.where(model._events['sync'], {callback:model.setOriginalAttributes}), "setOriginalAttributes is bound to sync");
 });
+
+// On sync, original attributes are set.
+asyncTest("on sync with a saved model", function(){
+  expect(3);
+
+  var attributes = {
+  	id: 1,
+  	apple: 'banana',
+  	pear: 'blueberry'
+  };
+  var model = new this.Model(attributes);
+
+  model.on('sync', function() {
+  	deepEqual(attributes, model._originalAttributes, "orginalAttributes property is set");
+  	deepEqual(attributes, model._originalAttributes, "orginal attributes are set correctly");
+  	deepEqual(model.attributes, model._originalAttributes, "orginal attributes are set correctly");
+  	start();
+  });
+
+  model.trigger('sync');
+});
+
+// On destroy, the sync event to bind setting original models is unbound.
+asyncTest("on destroy", function(){
+  expect(1);
+
+  var attributes = {
+  	id: 1,
+  	apple: 'banana',
+  	pear: 'blueberry'
+  };
+  var model = new this.Model(attributes);  
+
+  model.on('destroy', function() {
+  	var callbacks = _.where(model._events['sync'], {callback:model.setOriginalAttributes});
+  	ok((!callbacks || callbacks.length === 0), "on destroy the setOriginalAttributes method is unbound");
+  	start();
+  });
+
+  model.trigger('destroy');
+});
+
+// Changing attributes after sync puts the model into a dirty state.
+asyncTest("after sync, if an attribute is changed", function(){
+  expect(1);
+
+  var attributes = {
+  	id: 1,
+  	apple: 'banana',
+  	pear: 'blueberry'
+  };
+  var model = new this.Model(attributes);  
+
+  model.on('sync', function() {
+  	model.set('apple', 'pear');
+  	ok(model.isDirty(), 'the model is in a dirty state.');
+  	start();
+  });
+
+  model.trigger('sync');
+});
+
+// Original attributes can be restored
+
+// Changing attributes after sync puts the model into a dirty state.
+asyncTest("after sync, if an attribute is changed", function(){
+  expect(2);
+
+  var attributes = {
+  	id: 1,
+  	apple: 'banana',
+  	pear: 'blueberry'
+  };
+  var model = new this.Model(attributes);  
+
+  model.on('sync', function() {
+  	// Dirty things up.
+  	model.set('apple', 'pear');
+  	ok(model.isDirty(), 'the model is in a dirty state.');
+  	// Now restore
+  	model.restoreOriginalAttributes();
+  	ok(!model.isDirty(), 'the model is not in a dirty state.');
+  	start();
+  });
+
+  model.trigger('sync');
+});
+
+
 
